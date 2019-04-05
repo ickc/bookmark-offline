2653e2d70abc3c57a46078c4f26d13dda2adbb55
Kolen Cheung <christian.kolen@gmail.com>
2019-04-05 02:05:30 -0700

    get_html.py: fix too many open files error

--- bin/get_html.py
+++ bin/get_html.py
@@ -7,6 +7,10 @@ import grequests
 import pandas as pd
 import numpy as np
 
+# see https://stackoverflow.com/a/50039149
+import resource
+resource.setrlimit(resource.RLIMIT_NOFILE, (110000, 110000))
+
 __version__ = '0.2'
 
 HEADERS = {
