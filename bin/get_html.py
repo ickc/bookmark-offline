7d9c40a24eee9ee203bbb4a4f05bb19f74a37f09
Kolen Cheung <christian.kolen@gmail.com>
2019-04-04 23:13:40 -0700

    Create get_html.py

---
+++ bin/get_html.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import argparse
+from pathlib import Path
+
+import requests
+import pandas as pd
+import numpy as np
+
+__version__ = '0.1'
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Accept-Encoding': 'none',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Connection': 'keep-alive'
+}
+
+
+def get_html(url):
+    return requests.get(url, headers=HEADERS).content.decode('utf-8')
+
+
+def main(path, output):
+    df = pd.read_hdf(path)
+
+    # if output already existed, updates:
+    if Path(output).is_file():
+        df_old = pd.read_hdf(output)
+
+        # merging dfs
+        df_merged = df.merge(df_old[['html']], how='outer', left_index=True, right_index=True)
+
+        na_idx = df_merged.html.isna()
+
+        # fetch html
+        df_merged.loc[na_idx, 'html'] = df_merged[na_idx].index.map(get_html)
+
+        df = df_merged
+    else:
+        df['html'] = df.index.map(get_html)
+
+    df.to_hdf(
+        output,
+        'df',
+        format='table',
+        complevel=9,
+        fletcher32=True
+    )
+
+
+def cli():
+    parser = argparse.ArgumentParser(description="Save url content in HDF5.")
+
+    parser.add_argument('input', help='Input urls in HDF5.')
+    parser.add_argument('-o', '--output', help='Output HDF5. Update file if exists.')
+
+    parser.add_argument('-v', '--version', action='version',
+                        version='%(prog)s {}'.format(__version__))
+
+    args = parser.parse_args()
+
+    main(args.input, args.output)
+
+
+if __name__ == "__main__":
+    cli()
